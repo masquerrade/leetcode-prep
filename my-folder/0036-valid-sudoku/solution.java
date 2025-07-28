0ccdc37acbeebbf830f0f3eaca49b8c8e33d0858
Mayank Kumar <60103730+masquerrade@users.noreply.github.com>
2025-07-28 07:52:14 +0000

    [LeetCode Sync] Runtime - 2 ms (69.31%), Memory - 44.4 MB (99.97%)

--- my-folder/0036-valid-sudoku/solution.java
+++ my-folder/0036-valid-sudoku/solution.java
@@ -7,11 +7,13 @@ class Solution {
         Set<Character> [] r =new HashSet[9];
         Set<Character> [] b =new HashSet[9];
 
+        //Initializing Hashset
         for(int i=0;i<9;i++){
             c[i]=new HashSet<>();
             r[i]=new HashSet<>();
             b[i]=new HashSet<>();
         }
+
 
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
@@ -28,7 +30,7 @@ class Solution {
                 if(!r[i].add(val)){
                     return false;
                 }
-
+                //Formaula to fill all the elements which come in the current row. All the boxes in the current row will be filled
                 int bi=(i/3)*3+j/3;
 
                 if(!b[bi].add(val)){
