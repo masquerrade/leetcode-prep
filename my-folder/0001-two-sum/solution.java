604e7e8c9865cd4cb67a92bea5f27cd8034421ce
Mayank Kumar <60103730+masquerrade@users.noreply.github.com>
2025-07-31 08:21:01 +0000

    [LeetCode Sync] Runtime - 2 ms (99.40%), Memory - 44.9 MB (99.95%)

--- my-folder/0001-two-sum/solution.java
+++ my-folder/0001-two-sum/solution.java
@@ -1,25 +1,16 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // Map<Integer,Integer> mp = new HashMap<> ();
-        // for(int i=0;i<nums.length;i++){
-        //     mp.put(nums[i],i);
-        // }
-        //int [] nums1=new int[] {1,2,5,6,1};
-        Map<Integer,Integer> mp =IntStream.range(0,nums.length).boxed().collect(Collectors.toMap(i->nums[i],i->i,(a,b)->b));
-
+        Map<Integer,Integer> hm=new HashMap<>();
 
         for(int i=0;i<nums.length;i++){
-            if(mp.containsKey(target-nums[i])){
-                //If required value and current value are same it will give wrong result
-                if(mp.get(target-nums[i])!=i){
-                return new int[] {i,mp.get(target-nums[i])};
-                }
+            int comp=target-nums[i];
+            if(hm.containsKey(comp)){
+                //Way to return a new array
+                return new int[] {i,hm.get(comp)};
             }
+            //Check what will happenn if same value and complement
+            hm.put(nums[i],i);
         }
-
-        //Like this we can return empty array
-        return new int[] {-1,-1};
+        return new int[]{};
     }
-
-    
 }
