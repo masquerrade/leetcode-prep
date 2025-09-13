c7fe33a9b392bd55619c16f888757b58bd8deea8
Mayank Kumar <60103730+masquerrade@users.noreply.github.com>
2025-09-13 00:20:11 +0000

    [LeetCode Sync] Runtime - 891 ms (92.53%), Memory - 0.0B (100.00%)

--- my-folder/0185-department-top-three-salaries/solution.sql
+++ my-folder/0185-department-top-three-salaries/solution.sql
@@ -1,7 +1,51 @@
--- Write your PostgreSQL query statement below
-SELECT Department,Employee,Salary FROM
-(SELECT d.name AS Department,e.name as Employee,e.salary as Salary,
-DENSE_RANK() OVER (PARTITION BY d.name ORDER BY e.salary DESC) as rank
-FROM  Employee e LEFT JOIN Department d ON
-e.departmentId=d.id)
-WHERE rank<=3
+# Write your MySQL query statement below
+#What I'm planning to do .
+#I'll create a modified table with the extra rank column
+#Then will join this column with the other table or use this table 
+-- | Column Name  | Type    |
+-- +--------------+---------+
+-- | id           | int     |
+-- | name         | varchar |
+-- | salary       | int     |
+-- | departmentId | int     |
+
+-- Department
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | name        | varchar |
+
+-- +------------+----------+--------+
+-- | Department | Employee | Salary |
+-- +------------+----------+--------+
+-- | IT         | Max      | 90000  |
+-- | IT         | Joe      | 85000  |
+-- | IT         | Randy    | 85000  |
+-- | IT         | Will     | 70000  |
+-- | Sales      | Henry    | 80000  |
+-- | Sales      | Sam      | 60000  |
+
+
+SELECT 
+d.name AS Department,e.name AS Employee,e.salary as Salary
+FROM
+
+( SELECT *, 
+DENSE_RANK() OVER (PARTITION BY departmentId ORDER BY salary DESC) AS rnk
+ FROM Employee) e
+
+-- Here what I'm doing is creating a sub table using window function where I'm adding the additional info needed 
+-- Because  FROM clause is evaluated first
+
+JOIN Department d
+
+ ON e.departmentId=d.id
+
+ WHERE e.rnk<=3
+
+-- SELECT *, 
+-- DENSE_RANK() OVER (PARTITION BY departmentId ORDER BY salary DESC) AS rnk
+--  FROM Employee
+
