255bbb1fabc13aa23baa8cb1b19c7085cdd5fe77
Mayank Kumar <60103730+masquerrade@users.noreply.github.com>
2025-09-23 11:33:56 +0000

    [LeetCode Sync] Runtime - 2 ms (35.22%), Memory - 41.4 MB (99.95%)

---
+++ my-folder/0377-combination-sum-iv/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int combinationSum4(int[] nums, int target) {
+
+        int [] dp=new int[target+1];
+        dp[0]=1;
+
+        for(int i=1;i<=target;i++){
+            for(int num :nums){
+                //For current iteration i is my target
+                if(num<=i){
+                    dp[i]=dp[i]+dp[i-num];
+                }
+            }
+        }
+
+
+        return dp[target];
+        
+    }
+}
