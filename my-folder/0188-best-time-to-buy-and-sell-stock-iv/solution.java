cc9b4f561ba6bd1bb78e99225cabde09cda3246c
Mayank Kumar <60103730+masquerrade@users.noreply.github.com>
2025-09-27 13:26:18 +0000

    [LeetCode Sync] Runtime - 1 ms (99.91%), Memory - 41.7 MB (99.98%)

--- my-folder/0188-best-time-to-buy-and-sell-stock-iv/solution.java
+++ my-folder/0188-best-time-to-buy-and-sell-stock-iv/solution.java
@@ -1,59 +1,45 @@
 class Solution {
     public int maxProfit(int k, int[] prices) {
 
-        //Mainitain the state and 
-        //Two events can happen on daily basis either buy or sell And this can happen k times
-        //So we need to make a record of ith buy and ith sell
-        //So we are maintaining this in two arrays
-        //And we will update the whole array on daily basis
+        //Each day I will change the state of each transaction
 
-        //Unlimited transaction
-        if(k>prices.length/2){
+        //What is the current days price
 
-            int maxP=0;
+        // With this price if I made k transaction what all can I change
+         //k = 2, prices = [2,4,1]
+        
+        //We don't need only one array to store the states . We need two arrays  one to store the buy state and another to store the sell state
+         int [] buy=new int[k+1];
 
-            for(int i=1;i<prices.length;i++){
-                if(prices[i-1]<prices[i]){
-                    maxP+=(prices[i]-prices[i-1]);
-                }
+         Arrays.fill(buy,Integer.MIN_VALUE);
+         buy[0]=0;
+
+         int [] sell=new int[k+1];
+
+
+
+
+
+        for(int p:prices){
+            //What will happen if I do all k transactions using this price
+            //  [2,4,1]
+            // System.out.println("For current price "+p);
+            for(int i=1;i<=k;i++){
+                // System.out.println("After "+i+"th transaction");
+                //Buying today
+                buy[i]=Math.max(buy[i],sell[i-1]-p);
+                // System.out.println("After buying today");
+                // System.out.println(Arrays.toString(buy));
+                //Buying today
+                //Selling today
+                sell[i]=Math.max(sell[i],buy[i]+p);
+                // System.out.println("After selling today");
+                // System.out.println(Arrays.toString(sell));
+
             }
 
-            return maxP;
-
-        }
-        else{
-
-            
-        //k+1 as the first index is only used as a placeholder
-        int [] bdp=new int [k+1];
-        Arrays.fill(bdp,Integer.MIN_VALUE);
-        int [] sdp=new int [k+1];
-
-        bdp[0]=-prices[0];
-
-        for(int price:prices){
-
-            for(int i=1;i<=k;i++){//k times
-            //In one iteration I'm still in the same day
-            //1st buy profit is stored in the 1st index
-            bdp[i]=Math.max(bdp[i],sdp[i-1]-price);
-            //1st sell profit is stored in the 1st index
-            sdp[i]=Math.max(sdp[i],bdp[i]+price);
-        //     System.out.println(Arrays.toString(bdp));
-        // System.out.println(Arrays.toString(sdp));
         }
 
-
-        }
-        
-        // System.out.println(Arrays.toString(prices));
-
-        return sdp[k];       
-
-            
-        }
-
-
-        
+        return sell[k];
     }
 }
