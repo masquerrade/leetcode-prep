e513d4643f89353d8aa60c6695913ac55fcdd452
Mayank Kumar <60103730+masquerrade@users.noreply.github.com>
2025-09-27 12:59:04 +0000

    [LeetCode Sync] Runtime - 4 ms (49.91%), Memory - 61.1 MB (99.99%)

--- my-folder/0123-best-time-to-buy-and-sell-stock-iii/solution.java
+++ my-folder/0123-best-time-to-buy-and-sell-stock-iii/solution.java
@@ -1,33 +1,20 @@
 class Solution {
     public int maxProfit(int[] prices) {
-
-        //I've to maximize each day the profits in both transaction
-        int b1=-prices[0];
-        int s1=0;
+        //So if I have to solve this problem then what i wilk have to do lets see
+        //So I will maintain two variables in this case to maintain the current state one for transaction 1 and the other for transaction 2
+        int b1=Integer.MIN_VALUE;
         int b2=Integer.MIN_VALUE;
+        int s1=0;
         int s2=0;
 
-        for(int price:prices){
-
-            //max profit after first buy
-
-            b1=Math.max(b1,-price);
-
-            //max profit after first sell
-
-            s1=Math.max(s1,b1+price);
-
-            //max profit after second buy
-
-            b2=Math.max(b2,s1-price);
-
-            //Max profit after second sell
-            s2=Math.max(s2,b2+price);
-
+        for(int p:prices){
+            b1=Math.max(b1,-p);
+            s1=Math.max(s1,b1+p);
+            b2=Math.max(b2,s1-p);
+            s2=Math.max(s2,b2+p);
         }
 
         return s2;
-
-        
+            
     }
 }
