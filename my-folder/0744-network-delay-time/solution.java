1cea92420cd923dfc2ef678289d4cb7f6f435667
Mayank Kumar <60103730+masquerrade@users.noreply.github.com>
2026-04-15 22:38:25 +0000

    [LeetCode Sync] Runtime - 11 ms (65.43%), Memory - 49.3 MB (47.17%)

--- my-folder/0744-network-delay-time/solution.java
+++ my-folder/0744-network-delay-time/solution.java
@@ -1,94 +1,207 @@
-//Gathering the requirement
-
+//Attempt 3 Djkstra using list of list
+//Time 46 min
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
-
-        //If not weighted we can look at bfs
-        //I'm tracking the shortest distance in the directed graph so I will go bfs
-
-        //Adjacency List for graph
-
-        List<int[]>[] adj=new ArrayList[n+1];
-
-        //Fill the adj list
-        //This way we cannot fill array because of same reference problem
-        //Arrays.fill(adj,new ArrayList()<>);
-        for(int i=1;i<=n;i++){
-            adj[i]=new ArrayList<>();   
+        
+        //List to track the neighbours
+        //size n
+        List<List<int[]>> adj=new ArrayList<>(n+1);
+        //In my loop I need to pop the neighbours for aLL the nodes so need to initialize all
+        for(int i=0;i<=n;i++){
+            //adj.put(i,new ArrayList<int[]>()); There is no put function for the list
+            adj.add(i,new ArrayList<int[]>());
         }
 
-        for(int [] time:times){
-            //Here I just need to add . No need to overwrite
-            //adj[time[0]]=new ArrayList<>(Arrays.asList(time[0],time[1]));
-            adj[time[0]].add(new int[]{time[1],time[2]});
-            //What if some nodes are there with no neighbours then those will not be in the list
+        //Now I need to update the list with the neighbours
+        for(int []time:times){
+            int src=time[0];
+            int dst=time[1];
+            int wt=time[2];
+            adj.get(time[0]).add(new int[]{dst,wt});
         }
-
-        //Initialise the dist array to max value
-
+        
+        //Array to track the distance
         int[] dist=new int[n+1];
         Arrays.fill(dist,Integer.MAX_VALUE);
-
-        //Add kth node to the PQ
-        //Queue<int[]> bfs=new PriorityQueue((a,b)->Interger.compare(a[0],b[0]));
-        //Don't miss <>
-        Queue<int[]> bfs=new PriorityQueue<>((a,b)->a[0]-b[0]);
-        bfs.offer(new int[]{0,k });
-        //I need to update the shortest distance for the kth node
         dist[k]=0;
 
 
-        //Top node is the closest node in the queue
+        //Priority queue to track the closest node
+        //I need to use priority queue
+        //Queue<int []> pq=new ArrayDeque<int []>((a,b)->Integer.compare(a[0],b[0]));
+        Queue<int []> pq=new PriorityQueue<int []>((a,b)->Integer.compare(a[0],b[0]));
 
-        //Check the time taken to reach the neighbour node from current path.If for any of the neighbour a shorter time is found then I'll add it to the queue as we need to reprocess all it's neighbours
+        pq.offer(new int[]{0,k});
 
-        //What if one node comes in the neighbours list multiple times ,if it's total time is less then only will add to the queue
-
-        //When will the loop end ,when there are no more neighbours to add no elements remained in the queue
-
-        while(!bfs.isEmpty()){
-            int[] currNode=bfs.poll();
-            int currTime=currNode[0];
-            int currId=currNode[1];
-
-            //Before exploring the neighour we need to check if this node has already been updated to a lesser time by the time it was   popped
-            //Here = is not needed as it will skip processing the node in the queuq
-            if(currTime>dist[currId]){
-                continue;
-            }
+        //Dikstra loop
+        while(!pq.isEmpty()){
+            //Closest element 
+            int[] currNode=pq.poll();
             
-            //Exploring all the neighbours
-            //This is very common mistake adj is array of list so adj[currId] will be one list of array, so when iterating over the list its element will be array
-            for(int [] neighbour:adj[currId]){
-                int neighbourId=neighbour[0];
-                int newTime=currTime+neighbour[1];
-                if(newTime<dist[neighbourId]){
-                    dist[neighbourId]=newTime;
-                    bfs.offer(new int[]{newTime,neighbourId});
+            //Explore neighbours
+            for(int[]currNb:adj.get(currNode[1])){
+                int currW=currNb[1]+currNode[0];
+                if(currW<dist[currNb[0]]){
+                    dist[currNb[0]]=currW;
+                    pq.offer(new int[]{currW,currNb[0]});
                 }
             }
-
         }
 
-        //When this loop ends I will have all the minimum times for each node
-        int max=dist[1];
-        //Here we need to skip the 0th index so we need to use the normal loop
-        //for(int d:dist){
+        int maxW=-1;
+        //By now my dist array will have all the min dist
         for(int i=1;i<=n;i++){
             if(dist[i]==Integer.MAX_VALUE){
                 return -1;
             }
-            else if(dist[i]>max){
-                max=dist[i];
-            }
+            maxW=Math.max(maxW,dist[i]);
         }
 
-        return max;
-
-        //return (Arrays.stream(dist).anyMatch(x->x==Integer.MAX_VALUE))?-1:Arrays.stream(dist).max().orElse(-1);      
-
+        return maxW;
+        
     }
 }
+
+
+
+// //Bellman Ford approach
+// class Solution {
+//     public int networkDelayTime(int[][] times, int n, int k) {
+
+//         //What is the purpose of this for loop
+//         //Keep an array to track the least distance of the current node from the starting point
+//         int []dist =new int[n+1];
+
+//         Arrays.fill(dist,Integer.MAX_VALUE);
+//         dist[k]=0;
+
+//         //List of source ,target and the edge weight
+//         //Each edge I need to explore n-1 times
+//         //Question is if the source is not unreachable ,then I need to check the total distance for the destination node.
+//         //If it is less than the currntly present distance for the destination node I need to update it 
+//         //In first itertation only the node at which we are starting will be reachable ,and after that if we encounter any more nodes in that path it will be updated.
+//         //Outer loop will go n-1 times as maximum n-1 edges I need to relax
+//         for(int i=1;i<n;i++){
+
+//             //I need inner loop to iterate all the edges
+//             for(int[] time:times){
+//                 int src=time[0];
+//                 int dst=time[1];
+//                 int wt=time[2];
+//                 if(dist[src]!=Integer.MAX_VALUE && dist[src]+wt<dist[dst]){
+//                     //Update the dist 
+//                     dist[dst]=dist[src]+wt;
+//                 }
+//             }
+//         }
+
+//         //Now my distance array has the smallest distance of all the nodes from the starting point 
+//         int maxValue=-1;
+//         //Nodes are numbered from 1 to n so don't skip the nth node
+//         // for(int i=1;i<n;i++){
+//         for(int i=1;i<n+1;i++){
+
+//             if(dist[i]==Integer.MAX_VALUE){
+//                 return -1;
+//             }
+//             maxValue=Math.max(maxValue,dist[i]);
+//         }
+        
+//         return maxValue;
+//     }
+// }
+
+//Dijkstra approach
+
+// class Solution {
+//     public int networkDelayTime(int[][] times, int n, int k) {
+
+//         //If not weighted we can look at bfs
+//         //I'm tracking the shortest distance in the directed graph so I will go bfs
+
+//         //Adjacency List for graph
+
+//         List<int[]>[] adj=new ArrayList[n+1];
+
+//         //Fill the adj list
+//         //This way we cannot fill array because of same reference problem
+//         //Arrays.fill(adj,new ArrayList()<>);
+//         for(int i=1;i<=n;i++){
+//             adj[i]=new ArrayList<>();   
+//         }
+
+//         for(int [] time:times){
+//             //Here I just need to add . No need to overwrite
+//             //adj[time[0]]=new ArrayList<>(Arrays.asList(time[0],time[1]));
+//             adj[time[0]].add(new int[]{time[1],time[2]});
+//             //What if some nodes are there with no neighbours then those will not be in the list
+//         }
+
+//         //Initialise the dist array to max value
+
+//         int[] dist=new int[n+1];
+//         Arrays.fill(dist,Integer.MAX_VALUE);
+
+//         //Add kth node to the PQ
+//         //Queue<int[]> bfs=new PriorityQueue((a,b)->Interger.compare(a[0],b[0]));
+//         //Don't miss <>
+//         Queue<int[]> bfs=new PriorityQueue<>((a,b)->a[0]-b[0]);
+//         bfs.offer(new int[]{0,k });
+//         //I need to update the shortest distance for the kth node
+//         dist[k]=0;
+
+
+//         //Top node is the closest node in the queue
+
+//         //Check the time taken to reach the neighbour node from current path.If for any of the neighbour a shorter time is found then I'll add it to the queue as we need to reprocess all it's neighbours
+
+//         //What if one node comes in the neighbours list multiple times ,if it's total time is less then only will add to the queue
+
+//         //When will the loop end ,when there are no more neighbours to add or no elements remained in the queue
+
+//         while(!bfs.isEmpty()){
+//             int[] currNode=bfs.poll();
+//             int currTime=currNode[0];
+//             int currId=currNode[1];
+
+//             //Before exploring the neighour we need to check if this node has already been updated to a lesser time by the time it was   popped
+//             //Here = is not needed as it will skip processing the node in the queuq
+//             if(currTime>dist[currId]){
+//                 continue;
+//             }
+            
+//             //Exploring all the neighbours
+//             //This is very common mistake adj is array of list so adj[currId] will be one list of array, so when iterating over the list its element will be array
+//             for(int [] neighbour:adj[currId]){
+//                 int neighbourId=neighbour[0];
+//                 int newTime=currTime+neighbour[1];
+//                 if(newTime<dist[neighbourId]){
+//                     dist[neighbourId]=newTime;
+//                     bfs.offer(new int[]{newTime,neighbourId});
+//                 }
+//             }
+
+//         }
+
+//         //When this loop ends I will have all the minimum times for each node
+//         int max=dist[1];
+//         //Here we need to skip the 0th index so we need to use the normal loop
+//         //for(int d:dist){
+//         for(int i=1;i<=n;i++){
+//             if(dist[i]==Integer.MAX_VALUE){
+//                 return -1;
+//             }
+//             else if(dist[i]>max){
+//                 max=dist[i];
+//             }
+//         }
+
+//         return max;
+
+//         //return (Arrays.stream(dist).anyMatch(x->x==Integer.MAX_VALUE))?-1:Arrays.stream(dist).max().orElse(-1);      
+
+//     }
+// }
 
 
 // class Solution {
@@ -214,4 +327,4 @@ class Solution {
 
         
 //     }
-// }
+// } 
