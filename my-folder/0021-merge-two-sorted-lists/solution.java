1128d22be069c03890b8814df3f72b5eae6c6130
Mayank Kumar <60103730+masquerrade@users.noreply.github.com>
2026-04-25 23:23:45 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.3 MB (55.27%)

--- my-folder/0021-merge-two-sorted-lists/solution.java
+++ my-folder/0021-merge-two-sorted-lists/solution.java
@@ -11,35 +11,37 @@
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 
-        //
-        ListNode head1=list1;
-        ListNode head2=list2;
-        ListNode newList=new ListNode(-1);
-        ListNode currNode=newList;
+        //Compare the heads of both the list and make a new list
+        //Dummy node to track the head of the merged list easily
+        ListNode dummy=new ListNode(-1);
 
-        while(head1!=null && head2!=null){
-            //Comaparre value of curr node
-            if(head1.val<head2.val){
-                ListNode newNode=new ListNode(head1.val);
-                currNode.next=newNode;
-                head1=head1.next;
+        ListNode current =dummy;
+
+        while(list1!=null && list2!=null){
+            if(list1.val<list2.val){
+                current.next=list1;
+                current=current.next;
+                list1=list1.next;
             }
             else{
-                ListNode newNode=new ListNode(head2.val);
-                currNode.next=newNode;
-                head2=head2.next;
-            }
-            currNode=currNode.next;
-
+                current.next=list2;
+                current=current.next;
+                list2=list2.next;
+            }             
         }
-        if(head1==null){
-            currNode.next=head2;
+
+        if(list1==null){
+            current.next=list2;
         }
         else{
-            currNode.next=head1;
+            current.next=list1;
         }
 
-        return newList.next;
-        
+        return dummy.next;
+
     }
 }
+
+
+
+
