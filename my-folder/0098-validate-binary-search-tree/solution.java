5b1960a5b21ac134715b6812bd06a4ec076e192b
Mayank Kumar <60103730+masquerrade@users.noreply.github.com>
2026-05-17 13:01:08 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.6 MB (98.39%)

--- my-folder/0098-validate-binary-search-tree/solution.java
+++ my-folder/0098-validate-binary-search-tree/solution.java
@@ -13,76 +13,112 @@
  *     }
  * }
  */
- //Iterative inorder traversal gives the sorted list in binary search tree
+ //Recursive inorder traversal 
 class Solution {
     public boolean isValidBST(TreeNode root) {
 
-        //Why I need a stack ->Since it is a DFS  I need to reach the left most depth of the tree to find the smallest node
-
-        //This check is not needed as it will be handled in the while loop
-        // if(root == null){
-        //     return true;
-        // }
-
-        // Not valid definition Stack<Treenode> dfs=new ArrayDeque<>();
-        Deque<TreeNode> inorder=new ArrayDeque<>();
-        TreeNode current = root;
+        //I have to take care of the state in each iteration
+        //We cannot use either Integer of Reference valible to pass the state as it is getting updated during the call.
         TreeNode prev=null;
-        
-        //No need to push here as we will push inside the while loop
-        // inorder.push(root);
-
-        //Traverse the full tree while filling the stack till it becomes empty
-        // while(!inorder.isEmpty() && current!=null/*To avoid first null*/)This will fail because inorder is empty in first iteration .So we process if there are nodes in inorder or current in not null
-        while(!inorder.isEmpty() || current!=null/*To avoid first null*/){
-            //This will effectively push right and root node to the stack
-            //inorder.push(current); This need to be inside the if condition to avoid pushing null
-
-            //This will not be the smallest node so no need to pop the parent immediately .I need to pop the leftmost node
-            // TreeNode current =inorder.pop();
-
-            //Reach the leftmost node for the dfs
-            while(current!=null){
-                inorder.push(current);
-                //While traversing left do I need to add these element to the stack ->Yes because I need to process these nodes later
-                //Here only I can check the condition and return accordingly
-                //If the node is valid then only it is added to the stack
-                //To break early
-                //This is not needed as we have a global check
-                // if(current.left!=null&&current.left.val>=current.val){
-                //     return false;
-                // }
-
-                current=current.left;
-            }
-
-            //Now I need to keep track of the previous node and compare with the next smallest node
-            current=inorder.pop();
-            if(prev!=null&& prev.val>=current.val){
-                return false;
-            }
-            prev=current;
-
-            //This is not needed as we have a global check
-            // if(current.right!=null){
-            //     if(current.val>=current.right.val){
-            //         return false;
-            //     }
-            //     //We can move this push to the beginning of the outer loop
-            //     // inorder.push(current);
-            // }
-
-            //Even if right is null I need to move to right 
-            //Making the current as null is the crucial part otherwise same left node will keep on getting inserted repeatedly
-            current=current.right;
-
-        }
-
-        //If all the nodes has been visited then return true
-        return true;        
+        //I need to pass the state array
+        Integer[] prevState=new Integer[1];
+        return isValid(root,prevState);
         
     }
+
+    public boolean isValid(TreeNode root, Integer[] prev){
+
+        if(root==null){
+            return true;
+        }
+
+        //I need to completely traverse the left of the suntree to update the prev in order to compare the current node with it 
+        if(isValid(root.left,prev)==false){
+            return false;
+        }
+
+        //Now left subtree is completely evaluated that it is valid and the prev would have beeen updated till now
+        if(prev[0]!=null && prev[0]>=root.val){
+            return false;
+        }
+
+        //Now update the prev
+        prev[0]=root.val;
+
+        return isValid(root.right,prev);
+    }
 }
+
+// //Iterative inorder traversal gives the sorted list in binary search tree
+// class Solution {
+//     public boolean isValidBST(TreeNode root) {
+
+//         //Why I need a stack ->Since it is a DFS  I need to reach the left most depth of the tree to find the smallest node
+
+//         //This check is not needed as it will be handled in the while loop
+//         // if(root == null){
+//         //     return true;
+//         // }
+
+//         // Not valid definition Stack<Treenode> dfs=new ArrayDeque<>();
+//         Deque<TreeNode> inorder=new ArrayDeque<>();
+//         TreeNode current = root;
+//         TreeNode prev=null;
+        
+//         //No need to push here as we will push inside the while loop
+//         // inorder.push(root);
+
+//         //Traverse the full tree while filling the stack till it becomes empty
+//         // while(!inorder.isEmpty() && current!=null/*To avoid first null*/)This will fail because inorder is empty in first iteration .So we process if there are nodes in inorder or current in not null
+//         while(!inorder.isEmpty() || current!=null/*To avoid first null*/){
+//             //This will effectively push right and root node to the stack
+//             //inorder.push(current); This need to be inside the if condition to avoid pushing null
+
+//             //This will not be the smallest node so no need to pop the parent immediately .I need to pop the leftmost node
+//             // TreeNode current =inorder.pop();
+
+//             //Reach the leftmost node for the dfs
+//             while(current!=null){
+//                 inorder.push(current);
+//                 //While traversing left do I need to add these element to the stack ->Yes because I need to process these nodes later
+//                 //Here only I can check the condition and return accordingly
+//                 //If the node is valid then only it is added to the stack
+//                 //To break early
+//                 //This is not needed as we have a global check
+//                 // if(current.left!=null&&current.left.val>=current.val){
+//                 //     return false;
+//                 // }
+
+//                 current=current.left;
+//             }
+
+//             //Now I need to keep track of the previous node and compare with the next smallest node
+//             current=inorder.pop();
+//             if(prev!=null&& prev.val>=current.val){
+//                 return false;
+//             }
+//             prev=current;
+
+//             //This is not needed as we have a global check
+//             // if(current.right!=null){
+//             //     if(current.val>=current.right.val){
+//             //         return false;
+//             //     }
+//             //     //We can move this push to the beginning of the outer loop
+//             //     // inorder.push(current);
+//             // }
+
+//             //Even if right is null I need to move to right 
+//             //Making the current as null is the crucial part otherwise same left node will keep on getting inserted repeatedly
+//             current=current.right;
+
+//         }
+
+//         //If all the nodes has been visited then return true
+//         return true;        
+        
+//     }
+// }
 
 
 // // //Passing the range for recursion
