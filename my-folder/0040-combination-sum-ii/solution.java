75f54fc561bb6314fdbdcd2908dfaab6b5ecda11
Mayank Kumar <60103730+masquerrade@users.noreply.github.com>
2026-07-01 07:47:24 +0000

    [LeetCode Sync] Runtime - 6 ms (73.58%), Memory - 45.2 MB (67.25%)

--- my-folder/0040-combination-sum-ii/solution.java
+++ my-folder/0040-combination-sum-ii/solution.java
@@ -1,48 +1,97 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        //Only one tine sort
-        Arrays.sort(candidates);
- 
-        List<List<Integer>> finalList=new ArrayList<>();
-        combinationSum2Helper(candidates,target,new ArrayList<Integer>(),0,finalList);
-        return finalList;
         
+        //Sort the candidates to remove duplicates
+        Arrays.sort(candidates);
+        //It's mandatory to initialize list before using it 
+        // List<List<Integer>> finalList;
+        List<List<Integer>> finalList=new ArrayList<>();
+
+        backtrack(candidates,target,0,new ArrayList<>(),finalList);
+
+        return finalList;
     }
 
-    public void combinationSum2Helper(int[] candidates, int target, List<Integer> crrCmb, int canInd,List<List<Integer>> finalList) {
-
-        //Base case
-        if(target==0){
-            finalList.add(new ArrayList<>(crrCmb));
-            return ;
-        }
-
-        if(target<0){
-            return ;
-        }
+    private void backtrack(int[] candidates,int remain,int startIndex,List<Integer> currentList,List<List<Integer>> finalList){
         
-        //No need to sort candidates again and again
-        //Arrays.sort(candidates);
+        if(remain==0){
+            finalList.add(new ArrayList<>(currentList));
+            return;
+        }
 
-        for(int i=canInd;i<candidates.length;i++){
-
-            //We can take duplicates for the first time 
-            crrCmb.add(candidates[i]);
-            //i+1 is passed as starting index so that each candidate is used only once
-            combinationSum2Helper(candidates,target-candidates[i],crrCmb,i+1,finalList);
-
-            //This check is necessary first to avoid index out of bounds exception
-            //This is used to skip duplicates
-            while((i+1)<candidates.length && candidates[i]==candidates[i+1]){
-                i++;
+        for(int i=startIndex; i<candidates.length; i++){
+            
+            //i>startIndex is there just to allow the first call in this level
+            if(i>startIndex && candidates[i]==candidates[i-1]){
+                continue;
             }
 
-            //Removing the last index to explore other possibilities
-            crrCmb.remove(crrCmb.size()-1);
+            //Now I got a new candidate . Check whether the new candidaate is greater than target and break
+            if(candidates[i]>remain){
+                break;
+            }
+
+            //Add current candidate to the currentList
+            currentList.add(candidates[i]);
+
+            //Call backtrack
+            backtrack(candidates,remain-candidates[i],i+1,currentList,finalList);
+
+            currentList.remove(currentList.size()-1);
 
         }
 
-        return ;
-        
+
+
     }
 }
+
+
+// class Solution {
+//     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+//         //Only one tine sort
+//         Arrays.sort(candidates);
+ 
+//         List<List<Integer>> finalList=new ArrayList<>();
+//         combinationSum2Helper(candidates,target,new ArrayList<Integer>(),0,finalList);
+//         return finalList;
+        
+//     }
+
+//     public void combinationSum2Helper(int[] candidates, int target, List<Integer> crrCmb, int canInd,List<List<Integer>> finalList) {
+
+//         //Base case
+//         if(target==0){
+//             finalList.add(new ArrayList<>(crrCmb));
+//             return ;
+//         }
+
+//         if(target<0){
+//             return ;
+//         }
+        
+//         //No need to sort candidates again and again
+//         //Arrays.sort(candidates);
+
+//         for(int i=canInd;i<candidates.length;i++){
+
+//             //We can take duplicates for the first time 
+//             crrCmb.add(candidates[i]);
+//             //i+1 is passed as starting index so that each candidate is used only once
+//             combinationSum2Helper(candidates,target-candidates[i],crrCmb,i+1,finalList);
+
+//             //This check is necessary first to avoid index out of bounds exception
+//             //This is used to skip duplicates
+//             while((i+1)<candidates.length && candidates[i]==candidates[i+1]){
+//                 i++;
+//             }
+
+//             //Removing the last index to explore other possibilities
+//             crrCmb.remove(crrCmb.size()-1);
+
+//         }
+
+//         return ;
+        
+//     }
+// }
