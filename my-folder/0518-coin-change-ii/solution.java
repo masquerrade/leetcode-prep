1cac625ec7ec31849bd64b006126c9379c345aac
Mayank Kumar <60103730+masquerrade@users.noreply.github.com>
2026-07-02 11:59:29 +0000

    [LeetCode Sync] Runtime - 7 ms (69.83%), Memory - 43.1 MB (76.61%)

---
+++ my-folder/0518-coin-change-ii/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+
+        //Fix the coin and find all the ways to make the amount one by one 
+
+        int[] dp=new int[amount+1];
+        //No of ways to make 0 is 1 that is to take 0 coins
+
+        dp[0]=1;
+
+        for(int c:coins){
+
+            for(int start=c;start<=amount;start++){
+                dp[start]=dp[start]+dp[start-c];
+            }
+        }
+
+        return dp[amount];
+        
+    }
+}
