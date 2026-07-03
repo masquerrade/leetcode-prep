8a4663847746fa45d80a07bd8bfe23d34c743838
Mayank Kumar <60103730+masquerrade@users.noreply.github.com>
2026-07-03 10:55:40 +0000

    [LeetCode Sync] Runtime - 3 ms (83.36%), Memory - 43.3 MB (84.14%)

--- my-folder/0139-word-break/solution.java
+++ my-folder/0139-word-break/solution.java
@@ -39,46 +39,84 @@
 
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-
-        //Since same word can be used multiple times in the squence it is a permutation or combination
-        //The words can be used multiple times in any order so it is unbound knapsack and will come in the inner loop
-
-
-
-        Set<String> dp=new HashSet<>();
-
-        //Add empty string to the set
-        dp.add("");
-
-        for(int i=0;i<=s.length();i++){
-            String currentString=s.substring(0,i);
-
-            for(String word:wordDict){
-                if(currentString.endsWith(word)){
+        // Instead of a Set of Strings, we use a boolean array of size N + 1.
+        // dp[i] being true means the prefix of length 'i' is valid.
+        boolean[] dp = new boolean[s.length() + 1];
+        
+        // Base case: equivalent to dp.add("")
+        dp[0] = true; 
+        
+        for (int i = 1; i <= s.length(); i++) {
+            // We can still look at the current prefix conceptually, but no need to 
+            // constantly substring it in the inner loop.
+            
+            for (String word : wordDict) {
+                // To avoid string slicing, we just check lengths and characters
+                int wordLen = word.length();
+                
+                // 1. Is the current prefix long enough to even contain this word?
+                // 2. Does the prefix end with this word?
+                // 3. Was the string VALID right before this word started? (equivalent to dp.contains(subString))
+                if (i >= wordLen) {
+                    // Calculate where the substring would have started
+                    int previousIndex = i - wordLen;
                     
-                    //Finally I've figured out the trick .I need to find out if my current string ends with the current word , then check if the remaining string is present in the dp set
-                    int wordIndex=currentString.lastIndexOf(word);
-                    String subString=currentString.substring(0,wordIndex);
-                    if(dp.contains(subString)){
-                        dp.add(currentString);
+                    if (dp[previousIndex] && s.substring(previousIndex, i).equals(word)) {
+                        dp[i] = true;
+                        break; // We found a valid breakdown for this prefix, skip other words
                     }
-
                 }
             }
-
         }
-
-        // System.out.println(dp);
-
-        if(dp.contains(s)){
-            return true;
-        }
-
-        return false;
-
         
+        // equivalent to dp.contains(s)
+        return dp[s.length()]; 
     }
 }
+
+
+// class Solution {
+//     public boolean wordBreak(String s, List<String> wordDict) {
+
+//         //Since same word can be used multiple times in the squence it is a permutation or combination
+//         //The words can be used multiple times in any order so it is unbound knapsack and will come in the inner loop
+
+
+
+//         Set<String> dp=new HashSet<>();
+
+//         //Add empty string to the set
+//         dp.add("");
+
+//         for(int i=0;i<=s.length();i++){
+//             String currentString=s.substring(0,i);
+
+//             for(String word:wordDict){
+//                 if(currentString.endsWith(word)){
+                    
+//                     //Finally I've figured out the trick .I need to find out if my current string ends with the current word , then check if the remaining string is present in the dp set
+//                     int wordIndex=currentString.lastIndexOf(word);
+//                     String subString=currentString.substring(0,wordIndex);
+//                     if(dp.contains(subString)){
+//                         dp.add(currentString);
+//                     }
+
+//                 }
+//             }
+
+//         }
+
+//         // System.out.println(dp);
+
+//         if(dp.contains(s)){
+//             return true;
+//         }
+
+//         return false;
+
+        
+//     }
+// }
 
 
 // class Solution {
