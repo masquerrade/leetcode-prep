17be92932b302397b6c4f6f54d1a725f0a0091da
Mayank Kumar <60103730+masquerrade@users.noreply.github.com>
2026-08-27 11:15:50 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 46.6 MB (70.22%)

--- my-folder/0142-linked-list-cycle-ii/solution.java
+++ my-folder/0142-linked-list-cycle-ii/solution.java
@@ -10,48 +10,107 @@
  * }
  */
 public class Solution {
-    public ListNode detectCycle(ListNode head) {
+    public ListNode detectCycle(ListNode head) 
+    {
 
-        //Phase 1 find the cycle 
+        //That mathematics I need to put here
+        //L + K + nC=2(L+K)
+        //L+K =nC
+        //L=nC-K
+        //L=( n-1)C+C-K
+
+        //Overall steps
+        //Base case
+        if(head==null || head.next==null){
+            //Don't return head as we need to return null in case of no cycle
+            return null;
+        }
+        
+
+        //First step is to find the intersection of slow and fast pointer if the cycle is present 
+        ListNode inter=findIntersection(head);
+        if(inter==null){
+            return inter;
+        }
+
+        //Second step is to implement the trick to find the starting point
+        ListNode start=head;
+
+        //Untill the inter and start meet
+        while(start!=inter){
+            start=start.next;
+            inter=inter.next;
+        }
+
+        return start;
+        
+    }
+
+    private ListNode findIntersection(ListNode head){
+
         ListNode fast=head;
         ListNode slow=head;
 
         while(fast!=null && fast.next!=null){
-            //IN the first iteration this will always be same
-            // if(fast==slow){
-            //     //Cycle found
-            //     break;
-            // }
-
             slow=slow.next;
             fast=fast.next.next;
 
             if(fast==slow){
-                //Cycle found
-                break;
+                return fast;
             }
         }
 
-        //Not always the fast pointer will be null when the pevious loop terminates .
-        // if(fast==null){
-        if(fast==null || fast.next==null){
-            return null;
-        }
+        //Don't return fast as in the case the while doesn't run it should return null
+        // return fast;
+        return null;
 
-
-        //Phase 2 find the starting node
-        fast=head;
-
-        //Definitely cycle is there
-        while(fast!=slow){
-            slow=slow.next;
-            fast=fast.next;
-        }
-
-        return fast;
-        
-    }
+    } 
 }
+
+
+// public class Solution {
+//     public ListNode detectCycle(ListNode head) {
+
+//         //Phase 1 find the cycle 
+//         ListNode fast=head;
+//         ListNode slow=head;
+
+//         while(fast!=null && fast.next!=null){
+//             //IN the first iteration this will always be same
+//             // if(fast==slow){
+//             //     //Cycle found
+//             //     break;
+//             // }
+
+//             slow=slow.next;
+//             fast=fast.next.next;
+
+//             if(fast==slow){
+//                 //Cycle found
+//                 break;
+//             }
+//         }
+
+//         //Not always the fast pointer will be null when the pevious loop terminates .
+//         // if(fast==null){
+//         if(fast==null || fast.next==null){
+//             return null;
+//         }
+
+
+//         //Phase 2 find the starting node
+//         fast=head;
+
+//         //Definitely cycle is there
+//         while(fast!=slow){
+//             slow=slow.next;
+//             fast=fast.next;
+//         }
+
+//         return fast;
+        
+//     }
+// }
 
 // //Hashset method
 // public class Solution {
