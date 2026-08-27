3ff41470d71dffa590c99a0ededa48ed0f127c0d
Mayank Kumar <60103730+masquerrade@users.noreply.github.com>
2026-08-27 17:56:43 +0000

    [LeetCode Sync] Runtime - 5 ms (41.04%), Memory - 46.6 MB (79.60%)

--- my-folder/0023-merge-k-sorted-lists/solution.java
+++ my-folder/0023-merge-k-sorted-lists/solution.java
@@ -10,58 +10,106 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if(lists.length==0 || lists==null){
+
+        //Overall steps
+        //Defence check
+        if(lists==null || lists.length==0){
             return null;
         }
 
-        int n=lists.length;
+        //Create a min heap to store the head of all the lists
+        //Lenght of pq is fixed so we can pass the length
 
-        //Merge 0,1 ->0 ;2,3->2 ; 
-        //For next iteration double the interval
+        PriorityQueue<ListNode> minHeap=new PriorityQueue<>(lists.length, (n1,n2)-> Integer.compare(n1.val,n2.val));
 
-        //Outer loop to expand the intervals everytime
-        for(int i=1;i<n;i*=2){
-            //Inner loop to merge two lists
-            // for(int j=0;j<n;j+=i*2){ This will fail in the case of odd length array
-            for(int j=0;j+i<n;j+=i*2){
-                lists[j]=mergeTwoLists(lists[j],lists[j+i]);
+        //Initialize the heap with the initial values
+
+        for(ListNode list:lists){
+            if(list!=null){
+                minHeap.offer(list);
             }
         }
 
-        return lists[0];
+        //Create a merge list to return the answert
+        ListNode dummy=new ListNode(-1);
+        ListNode merged=dummy;
 
+        //All the nodes in the heap must be merged
+        while(!minHeap.isEmpty()){
+            ListNode nextNode=minHeap.poll();
+
+            merged.next=nextNode;
+            merged=merged.next;
+
+            if(nextNode.next!=null){
+                minHeap.offer(nextNode.next);
+            }
+        }
+
+        ListNode finalMerged=dummy.next;
+
+        dummy.next=null;
+
+        return finalMerged;
+        
     }
-    public ListNode mergeTwoLists(ListNode list1,ListNode list2){
+}
+
+
+// class Solution {
+//     public ListNode mergeKLists(ListNode[] lists) {
+//         if(lists.length==0 || lists==null){
+//             return null;
+//         }
+
+//         int n=lists.length;
+
+//         //Merge 0,1 ->0 ;2,3->2 ; 
+//         //For next iteration double the interval
+
+//         //Outer loop to expand the intervals everytime
+//         for(int i=1;i<n;i*=2){
+//             //Inner loop to merge two lists
+//             // for(int j=0;j<n;j+=i*2){ This will fail in the case of odd length array
+//             for(int j=0;j+i<n;j+=i*2){
+//                 lists[j]=mergeTwoLists(lists[j],lists[j+i]);
+//             }
+//         }
+
+//         return lists[0];
+
+//     }
+//     public ListNode mergeTwoLists(ListNode list1,ListNode list2){
         
 
-        ListNode dummy=new ListNode(-1);
-        ListNode curr=dummy;
+//         ListNode dummy=new ListNode(-1);
+//         ListNode curr=dummy;
 
-        while(list1!=null && list2!=null){
-            if(list1.val<list2.val){
-                curr.next=list1;
-                list1=list1.next;
-                curr=curr.next;
-            }
-            else{
-                curr.next=list2;
-                list2=list2.next;
-                curr=curr.next;
-            }
-        }
+//         while(list1!=null && list2!=null){
+//             if(list1.val<list2.val){
+//                 curr.next=list1;
+//                 list1=list1.next;
+//                 curr=curr.next;
+//             }
+//             else{
+//                 curr.next=list2;
+//                 list2=list2.next;
+//                 curr=curr.next;
+//             }
+//         }
 
-        if(list1==null){
-            curr.next= list2;
-        }
+//         if(list1==null){
+//             curr.next= list2;
+//         }
 
-        if(list2==null){
-            curr.next= list1;
-        }
+//         if(list2==null){
+//             curr.next= list1;
+//         }
 
-        return dummy.next;
-    }
+//         return dummy.next;
+//     }
 
-}
+// }
 
 
 
